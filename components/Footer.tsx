7cdaeaed1d926ce837867997a70018010b8073ef
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2022-08-14 23:52:01 +0200

    Add links to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,8 +2,10 @@ import { Link } from "./Link";
 
 export const Footer = () => {
   return (
-    <div className="py-3 mt-auto">
+    <div className="flex gap-6 py-3 mt-auto">
       <Link onClick={() => {}}>About</Link>
+      <Link onClick={() => {}}>Notice</Link>
+      <Link onClick={() => {}}>Contact</Link>
     </div>
   );
 };
