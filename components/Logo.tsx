2199f84a8396acc39244bcddaf9b2673da657693
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2022-08-14 16:53:34 +0200

    Increase title size

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,7 +2,7 @@ import { Crown } from "./icons/Crown";
 
 export const Logo = () => {
   return (
-    <h1 className={`flex gap-1 items-center`}>
+    <h1 className="flex gap-1 items-center text-xl">
       <Crown width={20} />
       Royale Calculator
     </h1>
