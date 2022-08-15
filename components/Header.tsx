3985c1b76f3e04117298e8d1e67895bd790aa2ce
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2022-08-15 10:08:03 +0200

    Remove link from header logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,10 @@
-import Link from "next/link";
 import { Logo } from "./Logo";
 import { UserNav } from "./UserNav";
 
 export const Header = () => {
   return (
     <header className="py-3 items-center flex justify-between">
-      <Link href="/">
-        <Logo />
-      </Link>
+      <Logo />
       <UserNav />
     </header>
   );
