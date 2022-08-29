8835d662371e1185e7c3511dbe7f32f3370e9cbd
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2022-08-29 17:59:27 +0200

    Add icon to alert

--- components/Alert.tsx
+++ components/Alert.tsx
@@ -1,9 +1,15 @@
 import { ReactNode } from "react";
+import { Info } from "./icons/Info";
 
 interface AlertProps {
   children: ReactNode;
 }
 
 export const Alert = ({ children }: AlertProps) => {
-  return <div className="rounded-md bg-slate-200 p-4">{children}</div>;
+  return (
+    <div className="rounded-md bg-slate-200 text-slate-600 p-4 flex gap-2">
+      <Info width="20" className="text-slate-500" />
+      {children}
+    </div>
+  );
 };
