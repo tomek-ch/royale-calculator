b6567736906d800e6bbc9efc42bb723e62b70a1e
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2022-09-19 22:11:17 +0200

    Remove console.log

--- public/theme.js
+++ public/theme.js
@@ -3,7 +3,6 @@ if (
   (!("isDarkMode" in localStorage) &&
     window.matchMedia("(prefers-color-scheme: dark)").matches)
 ) {
-  console.log("is dark theme");
   document.documentElement.classList.add("dark");
 } else {
   document.documentElement.classList.remove("dark");
